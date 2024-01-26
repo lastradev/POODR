546736aa78c720f349147411447b8747acae7b21
Oscar Lastra <olastra@easybroker.com>
2024-01-26 01:11:24 -0600

    smelly code

--- 6.2.1-inheritance/lib/inheritance/bicycle.rb
+++ 6.2.1-inheritance/lib/inheritance/bicycle.rb
@@ -1,18 +1,32 @@
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :style, :size,
+              :tape_color,
+              :front_shock, :rear_shock
 
   def initialize(**opts)
+    @style = opts[:style]
     @size = opts[:size]
     @tape_color = opts[:tape_color]
+    @front_shock = opts[:front_shock]
+    @rear_shock = opts[:rear_shock]
   end
 
   # Every bike has the same defaults for
   # tire and chain size.
   def spares
-    {
-      chain: '11-speed',
-      tire_size: '23',
-      tape_color: tape_color
-    }
+    # Smelly code.
+    if style == :road
+      {
+        chain: '11-speed',
+        tire_size: '23',          # Millimeters.
+        tape_color: tape_color
+      }
+    else
+      {
+        chain: '11-speed',
+        tire_size: '2.1',         # Inches.
+        front_shock: front_shock
+      }
+    end
   end
 end
